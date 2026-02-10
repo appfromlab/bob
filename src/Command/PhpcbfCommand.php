c92a47198a23d24f4af4b4f037ec6de2b29a5f94
Louis Gan <124089730+louis-afl@users.noreply.github.com>
2026-02-10 20:01:53 +0800

    Add command to beautify code

---
+++ src/Command/PhpcbfCommand.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * PHP CodeSniffer Beautifier for Vendor Prefixed Command
+ *
+ * Applies PHP CodeSniffer beautification rules to the vendor-prefixed directory
+ * to ensure consistent code formatting.
+ *
+ * @package Appfromlab\Bob\Command
+ */
+
+namespace Appfromlab\Bob\Command;
+
+use Appfromlab\Bob\Helper;
+use Appfromlab\Bob\Composer\BatchCommands;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+use Composer\Command\BaseCommand;
+
+/**
+ * Beautify the vendor-prefixed composer folder
+ *
+ * Force success on exit because it will fail for Github Action.
+ */
+class PhpcbfCommand extends BaseCommand {
+
+	protected function configure(): void {
+		$this->setName( 'afl:bob:phpcbf' )
+			->setDescription( 'Beautify PHP files in plugin folders.' );
+	}
+
+	/**
+	 * Execute the command
+	 *
+	 * Beautifies code in the vendor-prefixed directory using PHP CodeSniffer.
+	 *
+	 * @param InputInterface  $input  The input interface.
+	 * @param OutputInterface $output The output interface.
+	 * @return int Exit code (always 0 for success).
+	 */
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
+
+		$output->writeln( '' );
+		$output->writeln( '<info>------ [START] ' . __CLASS__ . '</info>' );
+		$output->writeln( '' );
+
+		// Get configuration.
+		$config = Helper::getConfig();
+
+		$commands = array(
+			new Process(
+				array(
+					'php',
+					$config['paths']['plugin_vendor_dir'] . 'bin/phpcbf',
+					'--standard=' . $config['paths']['plugin_dir'] . '.phpcs.xml',
+					$config['paths']['plugin_dir'] . 'src',
+					$config['paths']['plugin_dir'] . 'config',
+					$config['paths']['plugin_file'],
+				),
+				$config['paths']['plugin_dir'],
+				array(
+					'AFL_BOB_EXIT_0' => true,
+				),
+			),
+		);
+
+		$exit_code = BatchCommands::run( $this->getApplication(), $commands, $output );
+
+		$output->writeln( '' );
+		$output->writeln( '<info>--- [END] ' . __CLASS__ . '</info>' );
+		$output->writeln( '' );
+
+		// Always exit 0 (force success because it may fail for Github Action).
+		return 0;
+	}
+}
